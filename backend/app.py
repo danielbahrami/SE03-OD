b71d5a1acc1b4dbff44fb3f6c9b5c5a36fa39cb8
Daniel Bahrami <daniel@bahrami.dk>
2021-12-03 17:59:02 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,9 @@
 from flask import Flask, request, redirect, jsonify
 import pymysql.cursors
 
-connection = pymysql.connect(host='db',
-user='root',
-password='',
-database='db',
-cursorclass=pymysql.cursors.DictCursor,)
-
 app = Flask(__name__)
+
+connection = pymysql.connect(host='db', user='root', password='', database='db', cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/person', methods = ['POST'])
 def person():
@@ -17,7 +13,7 @@ def person():
     with connection.cursor() as cur:
         cur.execute("INSERT INTO persons (firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
     connection.commit()
-    return redirect("https://localhost/select.html")
+    return redirect("https://localhost/select.html", 200)
 
 @app.route('/persons', methods = ['GET'])
 def persons():
@@ -25,7 +21,6 @@ def persons():
         cur.execute("SELECT * FROM persons")
         persons = cur.fetchall()
     return jsonify(persons)
-
 
 if __name__ == '__main__':
     app.run()
